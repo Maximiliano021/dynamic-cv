377e4da6bddb038a2579b58ca9929cb103062f42
Maximiliano021 <escuderomaximiliano99@gmail.com>
2026-01-23 11:21:44 -0300

    img e import hotkeypad

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: "https://maximiliano021.github.io",
-  base: "/dynamic-cv/",
+  base: "/dynamic-cv",
   integrations: [tailwind()],
 });
